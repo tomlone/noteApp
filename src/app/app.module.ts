9fac73e391991f1a79a83d51f449bbe34bd78c17
tomlone <p.saisundeepkumar@gmail.com>
2020-08-24 18:07:05 +0530

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule          }   from    '@angular/platform-browser';
+import { NgModule               }   from    '@angular/core';
+import { ReactiveFormsModule 	} 	from 	'@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule       }   from    './app-routing.module';
+import { AppComponent           }   from    './app.component';
+import { HomeComponent          }   from    './home/home.component';
+import { ContentComponent       }   from    './common/components/content/content.component';
+import { SidebarComponent       }   from    './common/components/sidebar/sidebar.component';
+import { ToolbarComponent       }   from    './common/components/toolbar/toolbar.component';
+import { NotesComponent         }   from    './common/components/notes/notes.component';
+import { TruncateTextPipe       }   from    './common/pipes/truncate-text.pipe';
+import { FontAwesomeModule      }   from    '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations                :   [
+        AppComponent,
+        HomeComponent,
+        ContentComponent,
+        SidebarComponent,
+        ToolbarComponent,
+        NotesComponent,
+        TruncateTextPipe
+    ],
+    imports                     :   [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FontAwesomeModule
+    ],
+    providers                   :   [],
+    bootstrap                   :   [AppComponent]
 })
 export class AppModule { }
