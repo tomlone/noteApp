82956642645ae71b9f8e3ee5e77da3b2b1b46436
tomlone <p.saisundeepkumar@gmail.com>
2020-08-24 21:24:51 +0530

    toolbar component ui

--- src/app/common/components/toolbar/toolbar.component.ts
+++ src/app/common/components/toolbar/toolbar.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit      }   from    '@angular/core';
 
+import { faBars, 
+         faTrashAlt,									
+         IconDefinition,			
+         faPlusCircle			} 	from 	'@fortawesome/free-solid-svg-icons';
+
+import { NoteService            }   from    'src/app/common/services/note.service';
+
 @Component({
-    selector                    :   'app-toolbar',
+    selector                    :   'toolbar',
     templateUrl                 :   './toolbar.component.html',
     styleUrls                   :   ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
 
-    constructor() { }
+    faBars                      :   IconDefinition      =   faBars;
+    faDelete                    :   IconDefinition      =   faTrashAlt;
+    faCircle                    :   IconDefinition      =   faPlusCircle;          
+
+    constructor(
+        private noteService     :   NoteService
+    ) { }
 
     ngOnInit(): void {
+    }
+
+    public toggleMenu(): void {
+        this.noteService.isMenuOpened   =   !this.noteService.isMenuOpened;
+        console.log('menu toggle value -> ', this.noteService.isMenuOpened);
+    }
+
+    public createNotesTapped(): void {
+
+    }
+
+    public deleteNotesTapped(): void {
+
     }
 
 }
--- src/app/common/services/note.service.ts
+++ src/app/common/services/note.service.ts
@@ -5,5 +5,7 @@ import { Injectable             }   from    '@angular/core';
 })
 export class NoteService {
 
+    public isMenuOpened         :   boolean             =   true;
+
     constructor() { }
 }
