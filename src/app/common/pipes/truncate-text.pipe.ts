5ef494734b79081add6cce60c8b667bb2fd00295
tomlone <p.saisundeepkumar@gmail.com>
2020-08-24 21:03:12 +0530

    truncateText pipe code is added

--- src/app/common/pipes/truncate-text.pipe.ts
+++ src/app/common/pipes/truncate-text.pipe.ts
@@ -5,8 +5,27 @@ import { Pipe, PipeTransform    }   from    '@angular/core';
 })
 export class TruncateTextPipe implements PipeTransform {
 
-    transform(value: unknown, ...args: unknown[]): unknown {
-        return null;
-    }
+    transform(text: string, limit: number = 10, type?: string): string {
+
+		const additionalString		:	string				=	'...';
+		if(text) {
+            return text.length > limit ? text.substr(0, limit) + additionalString : text;
+			// if(text.length > limit) {
+			// 	return text.substr(0, limit) + additionalString;
+			// } else {
+			// 	return text;
+			// }
+		} else {
+			if(type === 'header') {
+				return 'New note header';
+			} 
+
+			if(type === 'text') {
+				return 'New note text';
+			}
+
+			return '';
+		}
+	}
 
 }
