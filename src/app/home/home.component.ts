d0d9b11cc199b34a11456c2d6f42bcacc1aae62c
tomlone <p.saisundeepkumar@gmail.com>
2020-08-24 21:50:33 +0530

    basic layout is done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit      }   from    '@angular/core';
+import { NoteService            }   from    '../common/services/note.service';
 
 @Component({
     selector                    :   'app-home',
@@ -7,9 +8,45 @@ import { Component, OnInit      }   from    '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    constructor() { }
+    public deviceSize           :   string;
+
+    constructor(
+        private noteService     :   NoteService
+    ) { }
 
     ngOnInit(): void {
+    }
+
+
+    private _getScreenSize(): any {
+        const viewPorts 		: 	any				=	[{
+			type				:	'xs',
+			minWidth			:	'(min-width: 0px)'
+		}, {
+			type				:	'sm',
+			minWidth			:	'(min-width: 600px)'
+		}, {
+			type				:	'md',
+			minWidth			:	'(min-width: 768px)'
+		}, {
+			type				:	'lg',
+			minWidth			:	'(min-width: 992px)'
+		}, {
+			type				:	'xl',
+			minWidth			:	'(min-width: 1200px)'
+        }];
+        
+        viewPorts.map((queryMedia: any) => {
+			if(window.matchMedia(queryMedia.minWidth).matches === true) {
+				this.deviceSize		=	queryMedia.type;
+				if(queryMedia.type === 'xs' || queryMedia.type === 'sm') {
+					this.noteService.isMenuOpened = false;
+				} else {
+					this.noteService.isMenuOpened = true;
+				}
+			}
+
+		});
     }
 
 }
